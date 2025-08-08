f93b33a41dbe9b1d5e784bf840b2d5abcd6cb37b
Marcelo Aragão <marcelofilho1987@gmail.com>
2025-08-08 19:00:46 -0300

    new commit

--- MAGEWeb/src/main.tsx
+++ MAGEWeb/src/main.tsx
@@ -15,15 +15,65 @@ createRoot(document.getElementById('root')!).render(
 let camera: THREE.PerspectiveCamera;
 let scene: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
-let points;
+let points,finalSphere;
 let geometry, material, material2;
+
+
+const BasicShader = {
+
+	name: 'BasicShader',
+
+	uniforms: {},
+
+	vertexShader: /* glsl */`
+			precision mediump float;
+			precision mediump int;
+
+			uniform mat4 modelViewMatrix; // optional
+			uniform mat4 projectionMatrix; // optional
+			uniform float var;
+
+			attribute vec3 position;
+			attribute vec4 color;
+
+			varying vec3 vPosition;
+			varying vec4 vColor;
+
+			void main()	{
+				vPosition = position * var;
+				vColor = color;
+
+				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+			}`,
+
+	fragmentShader: /* glsl */`
+			precision mediump float;
+			precision mediump int;
+
+			uniform float time;
+
+			varying vec3 vPosition;
+			varying vec4 vColor;
+
+			void main()	{
+
+				vec4 color = vec4( vColor );
+				color.r += sin( vPosition.x * 10.0 + time ) * 0.5;
+
+				gl_FragColor = color;
+
+			}`,
+
+};
+
 
 init();
 
 function init() {
 
 	camera = new THREE.PerspectiveCamera( 42, 1, 0.01, 10 );
-	camera.position.z = 1;
+	camera.position.z = 3;
 
 	scene = new THREE.Scene();
 	const radius = .1;
@@ -42,7 +92,53 @@ function init() {
     size: 3,       // in pixels
 	});
 	points = new THREE.Points(geometry, material);
-	scene.add(points);
+
+
+	// geometry
+	// nr of triangles with 3 vertices per triangle
+	const vertexCount = 200 * 3;
+
+	const geometryT = new THREE.BufferGeometry();
+
+	const positions = [];
+	const colors = [];
+
+	for ( let i = 0; i < vertexCount; i ++ ) {
+
+		// adding x,y,z
+		positions.push( Math.random() - 0.5 );
+		positions.push( Math.random() - 0.5 );
+		positions.push( Math.random() - 0.5 );
+
+		// adding r,g,b,a
+		colors.push( Math.random() * 255 );
+		colors.push( Math.random() * 255 );
+		colors.push( Math.random() * 255 );
+		colors.push( Math.random() * 255 );
+
+	}
+
+	const positionAttribute = new THREE.Float32BufferAttribute( positions, 3 );
+	const colorAttribute = new THREE.Uint8BufferAttribute( colors, 4 );
+
+	colorAttribute.normalized = true; // this will map the buffer values to 0.0f - +1.0f in the shader
+
+	geometryT.setAttribute( 'position', positionAttribute );
+	geometryT.setAttribute( 'color', colorAttribute );
+
+	const sphere = new THREE.SphereGeometry(radius,widthSegments, heightSegments);				
+	const sphereMaterial = new THREE.RawShaderMaterial( {
+					uniforms: {
+						time: { value: 1.0 }
+					},
+					vertexShader: BasicShader.vertexShader,
+					fragmentShader: BasicShader.fragmentShader,
+					side: THREE.DoubleSide,
+					transparent: false
+				} );
+
+	finalSphere = new THREE.Mesh(geometryT, sphereMaterial);
+	scene.add(finalSphere);
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setAnimationLoop( animation );
@@ -63,7 +159,14 @@ function animation( time: number ) {
 		sizeAttenuation: false,
 		size: 3,       // in pixels
 		});
-	
+
+	time = performance.now();
+
+	const object = scene.children[ 0 ];
+
+	object.rotation.y = time * 0.0005;
+	object.material.uniforms.time.value = time * 0.005;
+
 	renderer.render( scene, camera );
 
 }
@@ -111,5 +214,8 @@ export function mount( container ) {
 }
 
 export function updateCube(xPos: number) {
-  points.position.x = xPos;
+  	finalSphere.position.x = xPos;
+
+  	const object = scene.children[ 0 ];
+	object.material.uniforms.var = 0.0;
 }
